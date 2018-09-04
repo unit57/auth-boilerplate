42489a328b427eafe5c3d243a7430c72f2160868
Eric Sanchez <eric.sanchez@domandtom.com>
2018-09-04 16:30:42 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// starting point of application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth',{ useNewUrlParser: true })
+
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+// Server Setup
+const port  = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listenting on:', port);
